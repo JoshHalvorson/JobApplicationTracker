d984d51dd4d3f1c7f5f01a6c9b9319246406d4aa
JoshHalvorson <joshhalvorson16@gmail.com>
2019-11-21 12:54:10 -0800

    Remove todo

--- app/src/main/java/dev/joshhalvorson/jobapptracker/util/AddApplicationFormValidator.kt
+++ app/src/main/java/dev/joshhalvorson/jobapptracker/util/AddApplicationFormValidator.kt
@@ -18,8 +18,7 @@ class AddApplicationFormValidator  {
             if (companyName == "") {
                 companyNameWrong = true
             }
-
-            // TODO("Make sure date is in the right pattern")
+            
             val pattern = Pattern.compile("^(1[0-2]|0[1-9])/(3[01]|[12][0-9]|0[1-9])/[0-9]{4}$")
             if (dateApplied == "" || !pattern.matcher(dateApplied as CharSequence).matches()) {
                 dateAppliedWrong = true
